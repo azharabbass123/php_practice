963559efa7f3e0bb1f8bc7cdc1034775b97915ca
Azhar Abbass <azharabbass786.com@gmail.com>
2024-05-22 21:41:12 +0500

    Routing done.

--- dynamic_webpages/index.php
+++ dynamic_webpages/index.php
@@ -1,25 +1,12 @@
 <?php 
 
-require './Database.php';
 
-$heading = 'Home';
 
-// connecting database*
+ require 'Database.php';
 
-// $dsn = "mysql:host=localhost;dbname=blogpost_db;user=root";
-// $pdo = new PDO($dsn);
-
-// $statement = $pdo ->prepare('select * from posts');
-// $statement->execute();
-
-// $posts = $statement->fetchAll(PDO::FETCH_ASSOC);  
-
-// foreach($posts as $post){
-//   echo "<li>" . $post['title']. "</li>";
-// }
-
-//We can connect to database using Database class defined in another file
+// We can connect to database using Database class defined in another file
 $db = new Database();
 $post = $db->query('select title from posts where id = 2')->fetch(PDO::FETCH_ASSOC);
 echo ($post['title']);
-//include './views/index.view.php';
+
+require 'router.php';
---
+++ dynamic_webpages/router.php
@@ -0,0 +1,32 @@
+<?php 
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+
+$routes = [
+    '/'=> 'controlers/index.php',
+    '/about'=> 'controlers/about.php',
+    '/contact'=> 'controlers/contact.php',
+];
+
+
+
+function routeToControler($uri, $routes){
+
+    if(array_key_exists($uri, $routes)) {
+        require  $routes[$uri];
+    
+    } else {
+    
+       abort();
+    
+    }
+}
+
+routeToControler($uri, $routes);
+
+function abort($code = 404) {
+    http_response_code($code);
+
+    require "views/$code.php";
+}
