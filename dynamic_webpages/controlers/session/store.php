0073d2c5b7493cb91c9ca12efa03b1386265c738
Azhar Abbass <azharabbass786.com@gmail.com>
2024-05-30 02:32:36 +0500

    section-6 completed

--- dynamic_webpages/controlers/session/store.php
+++ dynamic_webpages/controlers/session/store.php
@@ -8,39 +8,15 @@ $form = LoginForm::validate($attributes = [
     'password' => $_POST['password']
 ]);
 
-$config = require('config.php');
-$db = new Database($config['database']);
+$signedIn = (new Authenticator)->attempt(
+    $attributes['email'], $attributes['password']
+);
 
-$errors = [];
-
-if(! Validater::string($email, 5, 50)) {
-    $errors['body'] = 'providea valid email address';
+if(! $signedIn){
+    $form->error(
+        'email', 'No matching account for that email address and password.'
+    )->throw();
 }
 
-if(! Validater::string($password)) {
-    $errors['body'] = 'Please provide a valid password ';
-}
-if(! empty($errors)){
-    require 'views/register/create.view.php';
-    exit();
-}
-
- $user = $db->query('select * from users where email = :email', [
-    'email'=> $email
-])->fetch();
-
-if($user){
-    print_r($user);
-    if (password_verify($password, $user['password'])){
-        $_SESSION['user'] = [
-            'email' => $email
-        ];
-        header('location: notes');
-        exit();
-    } else {
-        $errors['body'] = "Provided email and password does not match";
-        require 'views/session/create.view.php';
-    }
-} else{
-    require 'views/session/create.view.php';
-}
+header('location: /php_practice/dynamic_webpages/');
+exit();
